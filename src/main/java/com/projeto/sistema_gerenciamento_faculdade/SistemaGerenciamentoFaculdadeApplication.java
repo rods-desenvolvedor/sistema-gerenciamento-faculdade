8a00074222e1e1d0278052d3fb40fd5355550e4a
rods <rods.desenvolvedor@gmail.com>
2025-02-27 00:11:07 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/projeto/sistema_gerenciamento_faculdade/SistemaGerenciamentoFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_gerenciamento_faculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoFaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/sistema_gerenciamento_faculdade/SistemaGerenciamentoFaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.sistema_gerenciamento_faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoFaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
