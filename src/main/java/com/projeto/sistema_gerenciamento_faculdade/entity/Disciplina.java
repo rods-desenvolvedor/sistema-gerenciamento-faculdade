5c62f5c285e8e38cb53b9256d4fe70443c8d1834
rods <rods.desenvolvedor@gmail.com>
2025-02-27 12:28:25 -0300

    feat(disciplina): criação da entidade disciplina - base da entidade

---
+++ src/main/java/com/projeto/sistema_gerenciamento_faculdade/entity/Disciplina.java
@@ -0,0 +1,64 @@
+package com.projeto.sistema_gerenciamento_faculdade.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_disciplina")
+public class Disciplina {
+
+    public Disciplina()
+    {
+
+    }
+
+    public Disciplina(UUID id, String nome, int cargaHoraria) {
+        this.id = id;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nome;
+    private int cargaHoraria;
+
+    //private Professor professor;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    
+
+    
+    
+}
