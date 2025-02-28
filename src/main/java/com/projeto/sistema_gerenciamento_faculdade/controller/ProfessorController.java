8be16ee7040171c753b8c6932c606e423adb8546
rods <rods.desenvolvedor@gmail.com>
2025-02-28 17:38:49 -0300

    feat(professorController): adicionado o controller do professor - endpoint de cadastro

---
+++ src/main/java/com/projeto/sistema_gerenciamento_faculdade/controller/ProfessorController.java
@@ -0,0 +1,34 @@
+package com.projeto.sistema_gerenciamento_faculdade.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.sistema_gerenciamento_faculdade.dto.ProfessorRequestDto;
+import com.projeto.sistema_gerenciamento_faculdade.dto.ProfessorResponseDto;
+import com.projeto.sistema_gerenciamento_faculdade.entity.Professor;
+import com.projeto.sistema_gerenciamento_faculdade.service.ProfessorService;
+
+@RestController
+@RequestMapping("/professor")
+public class ProfessorController {
+    
+    private ProfessorService professorService;
+
+    ProfessorController(ProfessorService professorService)
+    {
+        this.professorService = professorService;
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<ProfessorResponseDto> cadastrarProfessor(@RequestBody ProfessorRequestDto professorRequestDto)
+    {
+        ProfessorResponseDto professorResponseDto = professorService.cadastrarProfessor(professorRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(professorResponseDto);
+    }
+
+
+}
