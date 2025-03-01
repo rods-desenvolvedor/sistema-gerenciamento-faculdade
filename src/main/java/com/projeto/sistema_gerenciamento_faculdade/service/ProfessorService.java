60b0349c22d19c1d97b6f40becc3a5907449fbba
rods <rods.desenvolvedor@gmail.com>
2025-03-01 19:05:47 -0300

    feat(professor): criação do service do professor - funções para cadastrar e listar professores

--- src/main/java/com/projeto/sistema_gerenciamento_faculdade/service/ProfessorService.java
+++ src/main/java/com/projeto/sistema_gerenciamento_faculdade/service/ProfessorService.java
@@ -1,5 +1,7 @@
 package com.projeto.sistema_gerenciamento_faculdade.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.projeto.sistema_gerenciamento_faculdade.dto.ProfessorRequestDto;
@@ -23,5 +25,12 @@ public class ProfessorService {
         Professor professor = professorRequestDto.toEntity();
         Professor savedProfessor = professorRepository.save(professor);
         return new ProfessorResponseDto(savedProfessor);
+    }
+
+    public List<ProfessorResponseDto> listarProfessores()
+    {
+        List<Professor> professors = professorRepository.findAll();
+
+        return professors.stream().map(ProfessorResponseDto::new).toList();
     }
 }
