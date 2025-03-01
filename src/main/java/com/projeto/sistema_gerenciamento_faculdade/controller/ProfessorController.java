91af7dc7409dab608ddb0431d3c3065d555af766
rods <rods.desenvolvedor@gmail.com>
2025-03-01 19:06:28 -0300

    feat(professor): criação do controller do professor - primeiros endpoints

--- src/main/java/com/projeto/sistema_gerenciamento_faculdade/controller/ProfessorController.java
+++ src/main/java/com/projeto/sistema_gerenciamento_faculdade/controller/ProfessorController.java
@@ -1,7 +1,10 @@
 package com.projeto.sistema_gerenciamento_faculdade.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,14 @@ public class ProfessorController {
         ProfessorResponseDto professorResponseDto = professorService.cadastrarProfessor(professorRequestDto);
         return ResponseEntity.status(HttpStatus.OK).body(professorResponseDto);
     }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<ProfessorResponseDto>> listarProfessores()
+    {
+        return ResponseEntity.ok(professorService.listarProfessores());
+    }
+
+
 
 
 }
