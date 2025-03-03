3764e9d11b2301683498ccb2db82689d5d08bf04
rods <rods.desenvolvedor@gmail.com>
2025-03-03 20:06:13 -0300

    feat(disciplina): endpoint para listar todas as disciplinas

--- src/main/java/com/projeto/sistema_gerenciamento_faculdade/controller/DisciplinaController.java
+++ src/main/java/com/projeto/sistema_gerenciamento_faculdade/controller/DisciplinaController.java
@@ -1,21 +1,20 @@
 package com.projeto.sistema_gerenciamento_faculdade.controller;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 import com.projeto.sistema_gerenciamento_faculdade.dto.DisciplinaRequestDto;
 import com.projeto.sistema_gerenciamento_faculdade.dto.DisciplinaResponseDto;
 import com.projeto.sistema_gerenciamento_faculdade.service.DisciplinaService;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/disciplina")
@@ -33,6 +32,13 @@ public class DisciplinaController {
     {
         DisciplinaResponseDto disciplinaResponseDto = disciplinaService.cadastrarDisciplina(disciplinaRequestDto, idProfessor);
         return ResponseEntity.status(HttpStatus.CREATED).body(disciplinaResponseDto);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<DisciplinaResponseDto>> listarDisciplinas()
+    {
+        List<DisciplinaResponseDto> disciplinas = disciplinaService.listarDisciplinas();
+        return ResponseEntity.ok().body(disciplinas);
     }
     
 }
